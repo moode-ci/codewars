00e41e4e26c3a32f9f0f5517df78812a4674859c
liyangyi <1156612970@qq.com>
2014-10-18 01:42:07 +0800

    lyy - resovel A B problem

--- 2014-10-12/yangyi-codewar.rb
+++ 2014-10-12/yangyi-codewar.rb
@@ -1,9 +1,15 @@
-# A:Remove Duplicates
+# A1:Remove Duplicates
 def unique(integers)
-  integers.uniq
+  integers_group = integers.group_by{|i| i}
+  integers_group.keys
 end
 
-# B:Change Machine
+#A2: Breaking chocolate problem
+def break_chocolate(n, m)
+  m*n > 0 ? m*n - 1 : 0
+end
+
+#B:Change Machine
 def change(cents)
   if cents <= 0
     {25 => 0, 10 => 0, 5 => 0, 1 => 0}
@@ -11,3 +17,5 @@ def change(cents)
     {25 => cents%25, 10 => cents%25%10, 5 => cents%25%10%5, 1 => cents%5}
   end
 end
+
+#C:Sherlock on numbers
